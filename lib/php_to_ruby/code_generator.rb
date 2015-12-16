e3cb5d2d12522f0387ab63b18e829c3387a92393
Michael Stämpfli <michael.staempfli@gmail.com>
2015-12-16 13:20:10 +0100

    code generation for the normal script

--- lib/php_to_ruby/code_generator.rb
+++ lib/php_to_ruby/code_generator.rb
@@ -1,4 +1,3 @@
-# require 'rltk/cg/contractor'
 require 'filigree/visitor'
 
 module PhpToRuby
@@ -11,8 +10,34 @@ module PhpToRuby
       end.join("\n")
     end
 
+    on FunctionDeclaration do |node|
+      result = ''
+      result << "def #{node.name}(#{visit node.argument})\n"
+      node.body.each do |statement|
+        result << visit(statement)
+        result << "\n"
+      end
+      result << "end\n"
+    end
+
+    on Echo do |node|
+      "puts #{visit node.expression}"
+    end
+
+    on Return do |node|
+      "return #{visit node.expression}"
+    end
+
     on Assignment do |node|
       "#{visit node.left} = #{visit node.right}"
+    end
+
+    on Call do |node|
+      "#{node.name}(#{visit node.argument})"
+    end
+
+    on InfixOperator do |node|
+      "#{visit node.left} #{node.name} #{visit node.right}"
     end
 
     on Variable do |node|
